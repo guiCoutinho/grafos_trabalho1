8216355a272c91eaf2f41fe6ccc7741f28606f7c
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2024-12-18 17:41:32 -0300

    Criaçao do arquivo Grafo.h e suas funçoes

---
+++ Trabalho1Grafos/include/Grafo.h
@@ -0,0 +1,30 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+
+class Grafo
+{
+    public:
+        Grafo();
+        virtual ~Grafo();
+
+        bool eh_bipartido();
+        int n_conexo();
+        int get_grau();
+        int get_ordem();
+        bool eh_direcionado();
+        bool vertice_ponderado();
+        bool aresta_ponderada();
+        bool eh_completo();
+        bool eh_arvore();
+        bool possui_articulacao();
+        bool possui_ponte();
+        void carrega_grafo();
+        vois novo_grafo();
+
+    protected:
+
+    private:
+};
+
+#endif // GRAFO_H
---
+++ Trabalho1Grafos/src/Grafo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Grafo.h"
+
+Grafo::Grafo()
+{
+    //ctor
+}
+
+Grafo::~Grafo()
+{
+    //dtor
+}
